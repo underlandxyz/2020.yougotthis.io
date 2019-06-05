1bcdb50570a389d9913e28ad054ce934b16921ad
Kevin Lewis <kvn@lws.io>
2019-06-05 16:05:32 +0200

    Meta info

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,10 +11,17 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'An affordable one-stream conference for juniors on skills needed for a happy, healthy work life.' }
+      { hid: 'description', name: 'description', content: 'An affordable one-stream conference for juniors on skills needed for a happy, healthy work life.' },
+      { property: 'og:image', content: 'https://yougotthis.io/og.png' },
+      { name: 'og:description', content: 'An affordable one-stream conference for juniors on skills needed for a happy, healthy work life.' }
     ],
     link: [
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-icon-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'manifest', href: '/manifest.json' },
       { rel: 'stylesheet', href: 'https://rsms.me/inter/inter-ui.css' }
     ]
   },
