23b665fb93a0fe9028ba97dc25a11c3600925df8
Kevin Lewis <kvn@lws.io>
2019-06-05 15:27:08 +0200

    Removed Brum from top

--- components/nav/TopBar.vue
+++ components/nav/TopBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="top-bar">
-    <span class="dates">January 18 2020. Birmingham, UK.</span>
+    <span class="dates">January 18 2020.</span>
+    <!-- <span class="dates">January 18 2020. Birmingham, UK.</span> -->
     <div class="meta">
       <n-link to='/sponsor'>Sponsor</n-link>
       <a href='mailto:ygt@underland.events'>Email Us</a>
