73096708b31ecf208f9b385100c8cbf5a4753d06
Kevin Lewis <kvn@lws.io>
2019-06-05 13:20:33 +0200

    CoC

---
+++ pages/conduct.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id='page' class='w'>
+    <h1 id="title">Underland Code of Conduct</h1>
+    <p>We believe that everyone has the right to be in a safe and welcoming environment when attending You Got This. This Code of Conduct is about creating an open and welcoming environment at our event so that all our attendees feel able to participate while being safe and happy.</p>
+    <p>People can have lots of viewpoints, and being part of this event means:</p>
+    <ul>
+      <li>Committing to helping each other</li>
+      <li>Listening to others' views</li>
+      <li>Setting an example</li>
+      <li>Treating everyone equally</li>
+      <li>Respecting your environment and the people around you</li>
+    </ul>
+    <h2>What is not acceptable</h2>
+    <p>To represent yourself to your very best ability, you must always practice:</p>
+    <ul>
+      <li>No violence</li>
+      <li>No harassment</li>
+      <li>No discrimination</li>
+      <li>No inappropriate imagery or content</li>
+    </ul>
+    <p>Harassment includes but is not limited to offensive verbal or written comments related to gender, age, sexual orientation, disability, physical appearance, body size, race, religion, sexual images in public spaces, deliberate intimidation, stalking, following, harassing photography or recording, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention. <b>If what you’re doing is making someone feel uncomfortable, that counts as harassment and is enough reason to stop doing it.</b></p>
+    <h2>Attendees asked to stop any harassing behaviour are expected to comply immediately.</h2>
+    <p>Absolutely everyone involved with our activities, including participants, sponsors, guests, vendors, volunteers and our organisers, are subject to our Code of Conduct.</p>
+    <p>If a person engages in any harassing behaviour, we may take any action we deem appropriate, including (but not limited to) warnings, expulsion, and banning, with no eligibility for reimbursement or refund of any type.</p>
+    <h2>What we won't respond to</h2>
+    <p>Our community priotises marginalised people's safety over privileged people's comfort. We will not act on complaints regarding:</p>
+    <ul>
+      <li>Reverse-isms, including reverse racism, reverse sexism, and cisphobia.</li>
+      <li>Reasonable communication of boundaries, such as "leave me alone", "go away", or "I'm not discussing this with you"</li>
+      <li>Refusal to explain or debate social justice concepts</li>
+    </ul>
+    <p>We expect people to follow these rules at all event venues should there be more than one.</p>
+    <h2>Reporting Code of Conduct violations</h2>
+    <p>If you are subject to, or see something, that makes you uncomfortable please tell us. No issue is too small, and we want to make sure you are safe and happy.</p>
+    <p>The best way to report bad behaviour is to call or text us at +44 7449 549 868. You can also is to email us at incidents@underland.events, or send us a DM on Twitter.</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#page {
+  margin-top: 2em;
+  margin-bottom: 2em;
+}
+* {
+  margin-bottom: 1rem;
+}
+ul {
+  margin-left: 1.25em;
+}
+</style>
