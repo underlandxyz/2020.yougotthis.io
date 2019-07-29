d7b24f9e06479ca684fb11a610c0d5227d992831
Kevin Lewis <kvn@lws.io>
2019-07-29 21:43:48 +0100

    Added Sponsors component and the Balsamiq card

---
+++ components/home/Sponsors.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id='sponsors' class='w'>
+    <h2>Made possible by</h2>
+    <div class="logos">
+      <div></div>
+      <a v-for='sponsor in sponsors' :key='sponsor.link' :href="sponsor.link">
+        <img :src="require(`@/assets/sponsors/${sponsor.image}`)" alt="">
+      </a>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sponsors: [
+        {
+          image: 'balsamiq.png',
+          link: 'https://balsamiq.com/'
+        },
+        {
+          image: 'you.png',
+          link: 'mailto:ygt@eunderland.events?subject=You Got This 2020 Sponsorship'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  text-align: center;
+}
+.logos {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 2em;
+  margin-top: 1em;
+}
+img {
+  width: 100%;
+}
+@media screen and (max-width: 600px) {
+  .logos {
+    grid-template-columns: 1fr 1fr;
+  }
+  .logos div {
+    display: none;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div id='page'>
     <Hero />
     <Intro />
+    <Sponsors />
     <WhatTheySay />
   </div>
 </template>
@@ -9,12 +10,14 @@
 <script>
 import Hero from '~/components/home/Hero';
 import Intro from '~/components/home/Intro';
+import Sponsors from '~/components/home/Sponsors';
 import WhatTheySay from '~/components/home/WhatTheySay';
 export default {
   components: {
     Hero,
     Intro,
-    WhatTheySay
+    WhatTheySay,
+    Sponsors
   }
 }
 </script>
