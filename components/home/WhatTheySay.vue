4bd785b76cb5ee460c5eac20fb2b6187e2c50051
Kevin Lewis <kvn@lws.io>
2019-09-05 18:39:01 -0400

    Updated highlights

--- components/home/WhatTheySay.vue
+++ components/home/WhatTheySay.vue
@@ -2,6 +2,7 @@
   <div id="highlights" class='w'>
     <h2>And here's what happened last year...</h2>
     <img src="~/assets/highlights/2019.png" alt="A selection of images from 2019's event">
+    <iframe src="https://www.youtube.com/embed/videoseries?list=PLhLrx4Pimiyk3jcO9kDhQtCRDKeRgv5zg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <div class="tweets">
       <div class="tweet">
         <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">I went to <a href="https://twitter.com/hashtag/YouGotThisConf?src=hash&amp;ref_src=twsrc%5Etfw">#YouGotThisConf</a> today and genuinely had such an amazing time. Met some lovely people, sat in on talks on subjects I resonated with such as anxiety, imposter syndrome and self-care</p><small>&mdash; Myles Adamson (@adamson_myles) <a href="https://twitter.com/adamson_myles/status/1086701099137011712?ref_src=twsrc%5Etfw">January 19, 2019</a></small></blockquote>
@@ -19,7 +20,7 @@
         <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">I had intended to leave the <a href="https://twitter.com/hashtag/YouGotThisConf?src=hash&amp;ref_src=twsrc%5Etfw">#YouGotThisConf</a> earlier in the afternoon to go work on my stem cells but the conference was SO GOOD I ended up staying all day! Thanks <a href="https://twitter.com/underlandxyz?ref_src=twsrc%5Etfw">@underlandxyz</a> for the great kickstart to 2019. Feeling motivated and inspired (eventho I&#39;m not a programmer) 😋🥰🌠 <a href="https://t.co/jZm3Yi63ww">pic.twitter.com/jZm3Yi63ww</a></p><small>&mdash; 🎆🌈Rachel Konichiwakitty🌟🎉 (@konichiwakitty) <a href="https://twitter.com/konichiwakitty/status/1086698393559318530?ref_src=twsrc%5Etfw">January 19, 2019</a></small></blockquote>
       </div>
       <a href='https://2019.yougotthis.io/highlights' class="more">
-        👉🏻  Find more, including talk recordings, on last year's site
+        👉🏻  See more highlights on last year's site
       </a>
     </div>
   </div>
@@ -37,6 +38,11 @@ h2 {
 img {
   width: 100%;
 }
+iframe {
+  margin-top: 2em;
+  width: 100%;
+  height: 500px;
+}
 .tweets {
   columns: 2;
   column-gap: 2em;
@@ -47,7 +53,6 @@ img {
       display: inline-block;
       border: 1px solid #e8e8e8;
       padding: 1em;
-      border-radius: 4px;
       small {
         display: block;
         margin-top: 1em;
@@ -58,10 +63,10 @@ img {
     background: var(--theme);
     color: white;
     padding: 0.75em;
-    border-radius: 4px;
     text-decoration: none;
     display: inline-block;
     font-size: 1.25em;
+    width: 100%;
   }
   @media screen and (max-width: 800px) {
     columns: 1;
