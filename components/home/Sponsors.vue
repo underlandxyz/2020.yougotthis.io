44c462d8db635e02a0c021bfe0d1e87f2d54be98
Kevin Lewis <kvn@lws.io>
2019-09-18 11:36:34 +0100

    Mozilla & GitHub

--- components/home/Sponsors.vue
+++ components/home/Sponsors.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id='sponsors' class='w'>
+  <div id="sponsors" class="w">
     <h2>Made possible by...</h2>
     <div class="logos">
-      <a v-for='sponsor in sponsors' :key='sponsor.link' :href="sponsor.link">
-        <img :src="require(`@/assets/sponsors/${sponsor.image}`)" alt="">
+      <a v-for="sponsor in sponsors" :key="sponsor.link" :href="sponsor.link">
+        <img :src="require(`@/assets/sponsors/${sponsor.image}`)" alt />
       </a>
     </div>
   </div>
@@ -15,8 +15,16 @@ export default {
     return {
       sponsors: [
         {
+          image: 'github.png',
+          link: 'https://github.com/'
+        },
+        {
           image: 'balsamiq.png',
           link: 'https://balsamiq.com/'
+        },
+        {
+          image: 'mozilla.png',
+          link: 'https://www.mozilla.org'
         },
         {
           image: 'twilio.png',
@@ -24,7 +32,8 @@ export default {
         },
         {
           image: 'you.png',
-          link: 'mailto:ygt@eunderland.events?subject=You Got This 2020 Sponsorship'
+          link:
+            'mailto:ygt@eunderland.events?subject=You Got This 2020 Sponsorship'
         }
       ]
     }
@@ -41,8 +50,8 @@ h2 {
 }
 .logos {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 2em;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 1.5em;
   margin-top: 1em;
 }
 img {
