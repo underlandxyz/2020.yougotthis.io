78685dbb4c4c65900ce30a4f6306b2eec1be0992
sunnycommits <srp.exe@gmail.com>
2019-12-16 10:22:07 +0000

    Scholarships reopened

--- pages/inclusion/kdi28dncnsv.vue
+++ pages/inclusion/kdi28dncnsv.vue
@@ -1,7 +1,7 @@
 <template>
   <div id='page' class='w'>
     <h1 id="title">Congratulations!</h1>
-    <p>We're really happy to be able to offer you a ticket-only scholarship to You Got This 2020. You can redeem your free ticket until December 22nd 2019.</p>
+    <p>We're really happy to be able to offer you a ticket-only scholarship to You Got This 2020. You can redeem your free ticket until December 31st 2019.</p>
     <p><a href="https://www.eventbrite.co.uk/e/you-got-this-2020-tickets-77737122827?discount=LOOKINGGLASS" class="cta">Click here to redeem your free ticket</a></p>
     <p>In return we'd love if you could please share the conference with #YouGotThisConf online to make sure more people know it's happening!</p>
   </div>
